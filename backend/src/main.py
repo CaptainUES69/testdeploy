322c5660b8322c0134d698f64ec18892312b8d0e
CaptainUES69 <69493833+CaptainUES69@users.noreply.github.com>
2025-04-12 09:06:47 +0500

    asd

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,12 @@
-import uvicorn
-
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 
 from .POST.router import router as POST
-from .GET.router import router as GET
 from .database.core import async_main
+
+import uvicorn
 
 
 @asynccontextmanager
@@ -25,8 +24,3 @@ app.add_middleware(
     allow_origins=["*"]
 )
 app.include_router(POST)
-app.include_router(GET)
-
-
-if __name__  ==  "__main__":
-    uvicorn.run("src.main:app", host = "127.0.0.1", port = 8000)
